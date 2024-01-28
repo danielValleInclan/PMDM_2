e9332e0eb269fc9632afb92eaf988f58c68bc5a5
danieldrv8 <danieldrv8@gmail.com>
2024-01-28 20:21:13 +0100

    Cambio nombre a ListaLugares.java

--- MisLugares/app/src/main/java/com/example/mislugares/repository/impl/RepositoryImpl.java
+++ MisLugares/app/src/main/java/com/example/mislugares/repository/impl/RepositoryImpl.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class ListaLugares implements LugaresRepository {
+public class RepositoryImpl implements LugaresRepository {
     private static final String DATABASE_NAME = "lugares.db";
     private static final int DATABASE_VERSION = 1;
 
@@ -39,7 +39,7 @@ public class ListaLugares implements LugaresRepository {
     private SQLiteDatabase database;
     private SQLiteOpenHelper dbHelper;
 
-    public ListaLugares(Context context) {
+    public RepositoryImpl(Context context) {
         dbHelper = new SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
             @Override
             public void onCreate(SQLiteDatabase db) {
